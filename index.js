f6e89f50380fbbfc15f2ac8768bb6dd1d5348772
Rafael <rgmllc@yahoo.com>
2023-09-03 22:59:28 -0400

    Update index.js
    Updated lambda URl

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const counter = document.querySelector(".counter-number");
 
 async function updateCounter() {
-    let response = await fetch("https://cqt4whz43svt2po7xpt7k3zoni0ortbr.lambda-url.us-east-1.amazonaws.com/");
+    let response = await fetch("https://cqt4whz43svt2po7xpt7k3zoni0ortbr.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = `Views: ${data}`;
 }
